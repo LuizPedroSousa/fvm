cc6cbdf642423ef7d429bea7f70af893b37e44bf
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-05-24 10:20:01 -0300

    chore(main): call 'what' function for logging :lipstick:

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,24 @@
 #include "application.hpp"
 
 int handleException(BaseException exception) {
-  std::cout << exception.message << std::endl;
+  std::cout << exception.what() << std::endl;
 
   return -1;
 }
 
 int main(int argc, char **argv) {
 
-  auto canProcess = Application::init();
+  try {
+    auto canProcess = Application::init();
 
-  if (canProcess.isLeft()) {
-    return handleException(canProcess.left());
+    if (canProcess.isLeft()) {
+      return handleException(canProcess.left());
+    }
+
+    Application::get()->run();
+  } catch (BaseException exception) {
+    return handleException(exception);
   }
-
-  Application::get()->run();
 
   return 0;
 }
