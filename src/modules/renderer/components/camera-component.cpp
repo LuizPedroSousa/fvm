138650f4dfaf8ce95d3c866254e5e5ebc61e1e86
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-05-24 09:52:20 -0300

    refact(renderer): extract camera into a component :recycle:

--- src/modules/renderer/components/camera-component.cpp
+++ src/modules/renderer/components/camera-component.cpp
@@ -1,4 +1,5 @@
-#include "./camera.hpp"
+#include "camera-component.hpp"
+#include "entities/object.hpp"
 
 #include "GLFW/glfw3.h"
 #include "glm/gtc/matrix_transform.hpp"
@@ -6,11 +7,13 @@
 #include "window.hpp"
 
 #include "application.hpp"
+#include "components/resource-component.hpp"
 #include "events/key-event.hpp"
 #include "events/mouse-event.hpp"
+#include "game.hpp"
 #include "iostream"
 
-Camera::Camera() {
+CameraComponent::CameraComponent() {
 }
 
 static float s_speed = 2.0f;
@@ -33,12 +36,10 @@ const float sensitivity = 0.1f;
 
 static bool first = true;
 
-Camera::Camera(unsigned int render_id, glm::vec3 position) {
-  m_uniform = Uniform(render_id);
-
-  m_transform = glm::mat4(1.0f);
+CameraComponent::CameraComponent(COMPONENT_INIT_PARAMS, glm::vec3 position) : COMPONENT_INIT(CameraComponent), m_is_orthographic(false) {
 
   s_position = position;
+
   m_position = s_position;
 
   KeyPressedDispatcher *key_pressed_dispatcher = KeyPressedDispatcher::get();
@@ -103,21 +104,43 @@ Camera::Camera(unsigned int render_id, glm::vec3 position) {
 
     s_front = glm::normalize(direction);
   });
+
+  glm::vec3 coral(1, 0.5f, 0.3f);
+  glm::vec3 light_color(1.0f, 1.0f, 1.0f);
 }
 
-void Camera::render() {
+void CameraComponent::use_orthographic() {
+  m_is_orthographic = true;
+}
+
+void CameraComponent::use_perspective() {
+  m_is_orthographic = false;
+}
+
+glm::mat4 CameraComponent::get_projection() {
+  // std::cout << m_is_orthographic << std::endl;
+  if (m_is_orthographic) {
+    return glm::ortho(0.0f, (float)Window::get_width(), 0.0f, (float)Window::get_height(), 0.1f, 100.0f);
+  } else {
+    return glm::perspective(45.0f, (float)Window::get_width() / (float)Window::get_height(), 0.1f, 100.0f);
+  }
+}
+
+glm::mat4 CameraComponent::get_view() {
   // set static positions
   m_position = s_position;
   m_front = s_front;
   m_up = s_up;
 
   // view space
-  m_transform = glm::mat4(1.0f);
-  m_transform = glm::lookAt(
+  auto transform = glm::mat4(1.0f);
+  transform = glm::lookAt(
       s_position, s_position + s_front, s_up);
 
-  glm::mat4 projection = glm::perspective(45.0f, (float)Window::get_width() / (float)Window::get_height(), 0.1f, 100.0f);
+  return transform;
+}
 
-  m_uniform.setMatrix("view", m_transform);
-  m_uniform.setMatrix("projection", projection);
+void CameraComponent::update(Uniform *uniform) {
+  uniform->setMatrix("view", get_view());
+  uniform->setMatrix("projection", get_projection());
 }
---
+++ src/modules/renderer/components/camera-component.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "components/base/component.hpp"
+#include "glm/glm.hpp"
+#include "resources/shader.hpp"
+#include "uniform.hpp"
+
+class CameraComponent : public Component<CameraComponent> {
+  public:
+  CameraComponent(COMPONENT_INIT_PARAMS, glm::vec3 position);
+  CameraComponent();
+
+  void update(Uniform *uniform);
+
+  void use_perspective();
+  void use_orthographic();
+
+  glm::vec3 get_position() { return m_position; };
+
+  private:
+  glm::mat4 get_projection();
+  glm::mat4 get_view();
+
+  glm::vec3 m_up;
+  glm::vec3 m_front;
+  unsigned int m_shader_render_id;
+  glm::vec3 m_position;
+
+  bool m_is_orthographic;
+};
