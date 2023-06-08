9cde0edf6cbc83890047da0520d97ffb782a0d43
luiz.pedro <luizpedrosousa64@gmail.com>
2023-06-08 14:20:42 -0300

    feat(resources): add material resource :sparkles:

---
+++ src/modules/renderer/resources/material.cpp
@@ -0,0 +1,7 @@
+#include "material.hpp"
+
+Material::Material(RESOURCE_INIT_PARAMS, std::vector<ResourceID> diffuses, std::vector<ResourceID> speculars) : RESOURCE_INIT(), diffuses(diffuses), speculars(speculars){};
+
+Material Material::create(ResourceID id, std::vector<ResourceID> diffuse_ids, std::vector<ResourceID> specular_ids) {
+  return Material(id, diffuse_ids, specular_ids);
+};
