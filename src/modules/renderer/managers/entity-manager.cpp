c2d97f3b5d361f4c74df99c0336e7cb4032fc0df
luiz.pedro <luizpedrosousa64@gmail.com>
2023-06-09 20:08:00 -0300

    perf(entity-manager): drop type_container and use unique_ptr instead of shared :zap:

--- src/modules/renderer/managers/entity-manager.cpp
+++ src/modules/renderer/managers/entity-manager.cpp
@@ -1,20 +1,24 @@
 #include "entity-manager.hpp"
 
-EntityManager::EntityManager(ComponentManager *component_manager) : m_component_manager(component_manager) {
-}
+EntityManager::EntityManager(ComponentManager *component_manager)
+    : m_component_manager(component_manager) {}
 
-EntityManager::~EntityManager() {
-}
+EntityManager::~EntityManager() {}
 
 void EntityManager::destroy_entity(const EntityID &entity_id) {
   const EntityTypeID type_id = m_entity_table[entity_id]->get_entity_type_id();
 
   m_component_manager->clean_components(entity_id);
 
-  m_entity_typed_table[type_id]->destroy_entity(entity_id);
   m_entity_table.erase(entity_id);
 }
 
 IEntity *EntityManager::get_entity(const EntityID &entity_id) {
-  return m_entity_table[entity_id].get();
+  auto it = m_entity_table.find(entity_id);
+
+  if (it != m_entity_table.end()) {
+    return it->second.get();
+  }
+
+  return nullptr;
 }
--- src/modules/renderer/managers/entity-manager.hpp
+++ src/modules/renderer/managers/entity-manager.hpp
@@ -5,57 +5,22 @@
 #include "unordered_map"
 #include "utils/guid.hpp"
 
-using IEntity_ptr = std::shared_ptr<IEntity>;
-
-class IEntityContainer {
-public:
-  virtual ~IEntityContainer() {}
-
-  virtual void destroy_entity(const EntityID &entity_id) = 0;
-};
-
-template <typename T> class EntityContainer : public IEntityContainer {
-  using TypedEntityContainer =
-      std::unordered_map<EntityID, std::shared_ptr<Entity<T>>>;
-
-private:
-  TypedEntityContainer m_typed_container;
-
-public:
-  EntityContainer() {}
-  TypedEntityContainer &get_typed_entity_container() {
-    return m_typed_container;
-  }
-  void destroy_entity(const EntityID &entity_id) override {
-    m_typed_container.erase(entity_id);
-  }
-};
+using IEntity_ptr = std::unique_ptr<IEntity>;
 
 class EntityManager {
 public:
   explicit EntityManager(ComponentManager *component_manager);
   ~EntityManager();
-
   template <typename T, typename... Args> T &add_entity(Args &&...params) {
     EntityID id = FamilyObjectID<IEntity>::get();
-    auto entity_ptr = std::make_shared<T>(id, this->m_component_manager,
+    auto entity_ptr = std::make_unique<T>(id, this->m_component_manager,
                                           std::forward<Args>(params)...);
-    auto created_entity = m_entity_table.try_emplace(id, entity_ptr);
 
-    ASSERT_THROW(!created_entity.second, "Error creating new Entity !");
+    auto created_entity = m_entity_table.emplace(id, std::move(entity_ptr));
 
-    auto container_ptr = get_entity_container<T>();
+    ASSERT_THROW(!created_entity.second, "Error creating new Entity!");
 
-    ASSERT_COMPARE_THROW(container_ptr);
-
-    auto container =
-        container_ptr.right()->get_typed_entity_container().try_emplace(
-            id, entity_ptr);
-
-    ASSERT_THROW(!container.second,
-                 "Error inserting Entity in typed containers !");
-
-    return *entity_ptr.get();
+    return dynamic_cast<T &>(*m_entity_table[id].get());
   }
 
   void destroy_entity(const EntityID &entity_id);
@@ -75,7 +40,7 @@ public:
     EntityTypeID type_id = T::entity_type_id();
 
     for (const auto &pair : m_entity_table) {
-      IEntity_ptr entity = pair.second;
+      const IEntity_ptr &entity = pair.second;
       if (entity->get_entity_type_id() == type_id) {
         result.push_back(dynamic_cast<T *>(entity.get()));
       }
@@ -84,31 +49,7 @@ public:
     return result;
   }
 
-  std::unordered_map<EntityID, IEntity_ptr> get_entities() {
-    return m_entity_table;
-  }
-
 private:
-  template <typename T>
-  Either<BaseException, EntityContainer<T> *> get_entity_container() {
-    EntityTypeID type_id = T::entity_type_id();
-
-    auto entity_typed = this->m_entity_typed_table.find(type_id);
-    if (entity_typed != m_entity_typed_table.end())
-      return reinterpret_cast<EntityContainer<T> *>(entity_typed->second.get());
-
-    auto entity_container = this->m_entity_typed_table.try_emplace(
-        type_id, std::make_unique<EntityContainer<T>>());
-
-    ASSERT(!entity_container.second, "Failed to create EntityContainer<T>!");
-
-    return reinterpret_cast<EntityContainer<T> *>(
-        entity_container.first->second.get());
-  }
-
-  using IEntityContainer_ptr = std::shared_ptr<IEntityContainer>;
-
-  std::unordered_map<EntityTypeID, IEntityContainer_ptr> m_entity_typed_table;
   std::unordered_map<EntityID, IEntity_ptr> m_entity_table;
   ComponentManager *m_component_manager;
 };
