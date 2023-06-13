90f33b64cadec9bf6563610b2a2a13973e2fcfd4
luiz.pedro <luizpedrosousa64@gmail.com>
2023-06-13 00:31:43 -0300

    feat(shared/base): add smart pointers and function bind helpers :sparkles:

---
+++ src/astralix/src/shared/base.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "memory"
+
+namespace astralix {
+
+#define ASTRA_BIND_EVENT_FN(fn)                                                \
+  [this](auto &&...args) -> decltype(auto) {                                   \
+    return this->fn(std::forward<decltype(args)>(args)...);                    \
+  }
+
+template <typename T> using Scope = std::unique_ptr<T>;
+template <typename T, typename... Args>
+constexpr Scope<T> create_scope(Args &&...args) {
+  return std::make_unique<T>(std::forward<Args>(args)...);
+};
+
+template <typename T> using Ref = std::shared_ptr<T>;
+template <typename T, typename... Args>
+constexpr Ref<T> create_ref(Args &&...args) {
+  return std::make_shared<T>(std::forward<Args>(args)...);
+};
+
+}; // namespace astralix
