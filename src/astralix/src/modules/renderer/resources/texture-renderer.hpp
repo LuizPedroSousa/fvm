06c8d9a9f58384b5e15ba192320874caf64b03fe
luiz.pedro <luizpedrosousa64@gmail.com>
2023-06-16 01:30:16 -0300

    feat(resources/texture-renderer): add cubemaps renderer  :sparkles:

--- src/astralix/src/modules/renderer/resources/texture-renderer.hpp
+++ src/astralix/src/modules/renderer/resources/texture-renderer.hpp
@@ -11,13 +11,15 @@ public:
   TextureRenderer(){};
   void render(Uniform *uniform);
 
-  void attach(ResourceID texture);
-  void attach_many(size_t size, ResourceID *textures);
+  void attach(ResourceID texture_id);
+  void attach_cubemap(ResourceID cubemap_ids);
+  void attach_many(size_t size, ResourceID *texture_ids);
 
   void start();
 
 private:
   std::vector<ResourceID> m_textures;
+  std::vector<ResourceID> m_cubemaps;
 };
 
 } // namespace astralix
