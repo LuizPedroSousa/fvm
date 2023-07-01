67d93d02a21d904848a2313bffd013a5a3bc63e7
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-07-01 18:05:47 -0300

    feat(renderer/layers): add properties layer for inspection :sparkles:

---
+++ src/astralix/src/modules/renderer/layers/properties-layer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "ecs/entities/ientity.hpp"
+#include "entities/layer.hpp"
+#include "entities/object.hpp"
+
+namespace astralix {
+
+class PropertiesLayer : public Layer {
+public:
+  PropertiesLayer() : Layer("Properties") {}
+
+  void start() override;
+  void update() override;
+
+private:
+  template <typename T, typename UIFunction>
+  static void draw_component(IEntity *entity, const char *icon_name,
+                             UIFunction uiFunction);
+
+  void draw_components(IEntity *entity);
+};
+
+} // namespace astralix
