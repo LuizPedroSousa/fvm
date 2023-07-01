98c82f39f24b5df8ca035e49d40bf9583eb38c0f
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-07-01 17:57:40 -0300

    chore(entities/scene): drop inheritance of entity :lipstick:

--- src/astralix/src/modules/renderer/entities/scene.hpp
+++ src/astralix/src/modules/renderer/entities/scene.hpp
@@ -1,15 +1,13 @@
 #pragma once
-#include "ecs/entities/entity.hpp"
 #include "either.hpp"
 
 #include "exceptions/base-exception.hpp"
 
 namespace astralix {
 
-class Scene : public Entity<Scene> {
+class Scene {
 public:
-  Scene(ENTITY_INIT_PARAMS) : ENTITY_INIT(){};
-  Scene() {}
+  Scene(){};
 
   virtual void start() = 0;
   virtual void update() = 0;
