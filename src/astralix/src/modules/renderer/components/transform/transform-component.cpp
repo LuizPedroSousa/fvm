0f1d2534c71f711be8195bb63862773f1c5e7445
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-07-01 17:46:19 -0300

    refact(components/transform-component): simplify calculation, getters, and setters of the transformation-matrix :recycle:

--- src/astralix/src/modules/renderer/components/transform/transform-component.cpp
+++ src/astralix/src/modules/renderer/components/transform/transform-component.cpp
@@ -7,62 +7,52 @@
 namespace astralix {
 
 TransformComponent::TransformComponent(COMPONENT_INIT_PARAMS,
-                                       glm::vec3 position, glm::vec3 scale)
-    : COMPONENT_INIT(TransformComponent) {
-  m_initial_transform = {
-      .position = position,
-      .scale = scale,
-      .rotation = glm::vec3(1.0f, 1.0f, 1.0f),
-      .rotation_angle = 0.0f,
-      .matrix = glm::mat4(1.0f),
-  };
+                                       glm::vec3 position, glm::vec3 scale,
+                                       glm::vec3 rotation, float rotation_angle)
+    : COMPONENT_INIT(TransformComponent, "Transform", false),
+      position(position), scale(scale), rotation(rotation),
+      rotation_angle(rotation_angle) {}
+
+void TransformComponent::start() {}
+
+void TransformComponent::recalculate_transform() {
+  glm::mat4 translation_matrix = glm::translate(glm::mat4(1.0f), position);
+  glm::mat4 scale_matrix = glm::scale(glm::mat4(1.0f), scale);
+  glm::mat4 rotatation_matrix =
+      glm::rotate(glm::mat4(1.0f), glm::radians(rotation_angle), rotation);
+
+  matrix = translation_matrix * rotatation_matrix * scale_matrix;
 }
 
-void TransformComponent::start() { m_render_transform = m_initial_transform; }
-
 void TransformComponent::update() {
-  m_render_transform.matrix = m_initial_transform.matrix;
+  recalculate_transform();
 
-  m_render_transform.matrix =
-      glm::translate(m_render_transform.matrix, m_render_transform.position);
-  m_render_transform.matrix =
-      glm::scale(m_render_transform.matrix, m_render_transform.scale);
-  m_render_transform.matrix =
-      glm::rotate(m_render_transform.matrix,
-                  glm::radians(m_render_transform.rotation_angle),
-                  m_render_transform.rotation);
-
-  auto owner = Engine::get()->get_entity_manager()->get_entity(get_owner_id());
+  auto owner = EntityManager::get()->get_entity(get_owner_id());
 
   auto resource = owner->get_component<ResourceComponent>();
 
   if (resource != nullptr) {
-    resource->get_shader_renderer()->get_uniform()->setMatrix(
-        "model", m_render_transform.matrix);
+    resource->get_shader_renderer()->get_uniform()->setMatrix("model",
+                                                              this->matrix);
   }
 }
 
-void TransformComponent::translate_initial(glm::vec3 position) {
-  m_initial_transform.position = position;
+void TransformComponent::translate(glm::vec3 p_position) {
+  this->matrix = glm::translate(this->matrix, p_position);
+  this->position = p_position;
 }
 
-void TransformComponent::scale_initial(glm::vec3 scale) {
-  m_initial_transform.scale = scale;
+void TransformComponent::set_scale(glm::vec3 p_scale) {
+  this->matrix = glm::scale(this->matrix, p_scale);
+  this->scale = p_scale;
 }
 
-void TransformComponent::translate(glm::vec3 position) {
-  m_render_transform.matrix =
-      glm::translate(m_render_transform.matrix, position);
-  m_render_transform.position = position;
-}
-
-void TransformComponent::scale(glm::vec3 scale) {
-  m_render_transform.matrix = glm::scale(m_render_transform.matrix, scale);
-  m_render_transform.scale = scale;
-}
-
-void TransformComponent::reset_render_transform() {
-  m_render_transform = m_initial_transform;
+void TransformComponent::reset_transform() {
+  this->position = glm::vec3(0.0f);
+  this->scale = glm::vec3(1.0f);
+  this->rotation = glm::vec3(1.0f, 1.0f, 1.0f);
+  this->rotation_angle = 0.0f;
+  this->matrix = glm::mat4(1.0f);
 }
 
 } // namespace astralix
--- src/astralix/src/modules/renderer/components/transform/transform-component.hpp
+++ src/astralix/src/modules/renderer/components/transform/transform-component.hpp
@@ -4,40 +4,28 @@
 
 namespace astralix {
 
-struct Transform {
+class TransformComponent : public Component<TransformComponent> {
+public:
+  TransformComponent(COMPONENT_INIT_PARAMS,
+                     glm::vec3 position = glm::vec3(0.0f),
+                     glm::vec3 scale = glm::vec3(1.0f),
+                     glm::vec3 rotation = glm::vec3(1.0f),
+                     float rotation_angle = 0.0f);
+
+  void set_scale(glm::vec3 p_scale);
+  void translate(glm::vec3 p_position);
+  void start();
+  void update();
+  void reset_transform();
+
   glm::vec3 position;
   glm::vec3 scale;
   glm::vec3 rotation;
   float rotation_angle;
-
   glm::mat4 matrix;
-};
-
-class TransformComponent : public Component<TransformComponent> {
-public:
-  TransformComponent(COMPONENT_INIT_PARAMS, glm::vec3 position,
-                     glm::vec3 scale);
-
-  TransformComponent(){};
-
-  void translate_initial(glm::vec3 position);
-  void scale_initial(glm::vec3 scale);
-
-  void scale(glm::vec3 scale);
-  void translate(glm::vec3 position);
-
-  void start();
-  void update();
-
-  Transform *get_render_transform() { return &m_render_transform; }
-
-  Transform *get_initial_transform() { return &m_initial_transform; }
-
-  void reset_render_transform();
 
 private:
-  Transform m_initial_transform;
-  Transform m_render_transform;
+  void recalculate_transform();
 };
 
 } // namespace astralix
