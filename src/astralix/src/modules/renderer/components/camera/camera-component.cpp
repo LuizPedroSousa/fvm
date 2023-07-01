95149bd8ac0422603ffa11e9ab703579f4c3d49b
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-07-01 17:49:30 -0300

    fix(camera-component): fix calculation of projection matrix with the framebuffer aspect-ratio and drop static global variables :bug:

--- src/astralix/src/modules/renderer/components/camera/camera-component.cpp
+++ src/astralix/src/modules/renderer/components/camera/camera-component.cpp
@@ -1,4 +1,5 @@
 #include "camera-component.hpp"
+#include "components/transform/transform-component.hpp"
 #include "entities/object.hpp"
 
 #include "GLFW/glfw3.h"
@@ -17,93 +18,78 @@
 
 namespace astralix {
 
-CameraComponent::CameraComponent() {}
-
-static float s_speed = 2.0f;
-static glm::vec3 s_position;
-static glm::vec3 s_up    = glm::vec3(0.0f, 1.0f, 0.0f);
-static glm::vec3 s_front = glm::vec3(0.003012, -0.509046, 0.860734);
-
-static int count = 0;
-
-static glm::vec3 direction;
-
-static float last_x, last_y;
-
-static float offset_x, offset_y;
-
-static float pitch = 0.0f;
-static float yaw   = -90.0f;
-
-const float sensitivity = 0.1f;
-
-static bool first = true;
-
-CameraComponent::CameraComponent(COMPONENT_INIT_PARAMS, glm::vec3 position)
-    : COMPONENT_INIT(CameraComponent), m_is_orthographic(false) {
-  s_position = position;
-  last_x     = Window::get()->get_height() / 2;
-  last_y     = Window::get()->get_width() / 2;
-  m_position = s_position;
+CameraComponent::CameraComponent(COMPONENT_INIT_PARAMS)
+    : COMPONENT_INIT(CameraComponent, "Camera", true),
+      m_is_orthographic(false) {
+  last_offset_x = Window::get()->get_height() / 2;
+  last_offset_y = Window::get()->get_width() / 2;
 
   EventDispatcher *event_dispatcher = EventDispatcher::get();
 
-  event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(KeyCode::W, []() {
-    count += 1;
-    s_position += s_speed * Time::get()->get_deltatime() * direction;
-  });
+  event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(
+      KeyCode::W, [&]() {
+        auto owner = get_owner();
 
-  event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(KeyCode::A, []() {
-    s_position -= s_speed * Time::get()->get_deltatime() *
-                  glm::normalize(glm::cross(s_front, s_up));
-  });
+        auto camera = owner->get_component<CameraComponent>();
+        auto transform = owner->get_component<TransformComponent>();
 
-  event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(KeyCode::S, []() {
-    s_position -= s_speed * Time::get()->get_deltatime() * direction;
-  });
-
-  event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(KeyCode::D, []() {
-    s_position += s_speed * Time::get()->get_deltatime() *
-                  glm::normalize(glm::cross(s_front, s_up));
-  });
+        transform->position +=
+            camera->speed * Time::get()->get_deltatime() * camera->direction;
+      });
 
   event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(
-      KeyCode::Space,
-      []() { s_position.y += Time::get()->get_deltatime() * 2.0f; });
+      KeyCode::A, [&]() {
+        IEntity *owner = get_owner();
+        auto transform = owner->get_component<TransformComponent>();
+        auto camera = owner->get_component<CameraComponent>();
+
+        transform->position -=
+            camera->speed * Time::get()->get_deltatime() *
+            glm::normalize(glm::cross(camera->front, camera->up));
+      });
 
   event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(
-      KeyCode::LeftControl,
-      []() { s_position.y -= Time::get()->get_deltatime() * 2.0f; });
+      KeyCode::S, [&]() {
+        IEntity *owner = get_owner();
+        auto transform = owner->get_component<TransformComponent>();
+        auto camera = owner->get_component<CameraComponent>();
 
-  event_dispatcher->attach<MouseListener, MouseEvent>([](MouseEvent *event) {
-    if (first) {
-      last_x = event->get_x();
-      last_y = event->get_y();
-      first  = false;
-    }
+        transform->position -=
+            camera->speed * Time::get()->get_deltatime() * camera->direction;
+      });
 
-    offset_x = event->get_x() - last_x;
-    offset_y = event->get_y() - last_y;
-    last_x   = event->get_x();
-    last_y   = event->get_y();
+  event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(
+      KeyCode::D, [&]() {
+        IEntity *owner = get_owner();
+        auto transform = owner->get_component<TransformComponent>();
+        auto camera = owner->get_component<CameraComponent>();
 
-    offset_x *= sensitivity;
-    offset_y *= sensitivity;
+        transform->position +=
+            camera->speed * Time::get()->get_deltatime() *
+            glm::normalize(glm::cross(camera->front, camera->up));
+      });
 
-    yaw += offset_x;
-    pitch -= offset_y;
+  event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(
 
-    // limit rotation of y axis
-    if (pitch > 89.0f)
-      pitch = 89.0f;
-    if (pitch < -89.0f)
-      pitch = -89.0f;
+      KeyCode::Space, [&]() {
+        auto transform = get_owner()->get_component<TransformComponent>();
+        transform->position.y += Time::get()->get_deltatime() * 2.0f;
+      });
 
-    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-    direction.y = sin(glm::radians(pitch));
-    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+  event_dispatcher->attach<KeyboardListener, KeyPressedEvent>(
+      KeyCode::LeftControl, [&]() {
+        auto owner = EntityManager::get()->get_entity<Object>(get_owner_id());
 
-    s_front = glm::normalize(direction);
+        auto transform = owner->get_component<TransformComponent>();
+        transform->position.y -= Time::get()->get_deltatime() * 2.0f;
+      });
+
+  event_dispatcher->attach<MouseListener, MouseEvent>([&](MouseEvent *event) {
+    IEntity *owner = get_owner();
+    auto transform = owner->get_component<TransformComponent>();
+    auto camera = owner->get_component<CameraComponent>();
+
+    camera->recalculate_camera_rotation(event->get_x(), event->get_y());
   });
 }
 
@@ -115,24 +101,55 @@ void CameraComponent::recalculate_projection_matrix() {
   if (m_is_orthographic) {
     m_projection_matrix = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f);
   } else {
-    auto window         = Window::get();
+
+    auto window = Window::get();
+    const auto &spec = Engine::get()->framebuffer->get_specification();
+
     m_projection_matrix = glm::perspective(
-        45.0f, (float)window->get_width() / (float)window->get_height(), 0.1f,
-        100.0f);
+        45.0f, (float)spec.width / (float)spec.height, 0.1f, 100.0f);
   }
 }
 
 void CameraComponent::recalculate_view_matrix() {
-  // set static positions
-  m_position = s_position;
-  m_front    = s_front;
-  m_up       = s_up;
-
   // view space
   auto matrix = glm::mat4(1.0f);
-  matrix      = glm::lookAt(s_position, s_position + s_front, s_up);
+
+  auto transform = get_owner()->get_component<TransformComponent>();
+
+  matrix = glm::lookAt(transform->position, transform->position + front, up);
 
   m_view_matrix = matrix;
+}
+
+void CameraComponent::recalculate_camera_rotation(float x, float y) {
+  if (is_first_recalculation) {
+    last_offset_x = x;
+    last_offset_y = y;
+    is_first_recalculation = false;
+  }
+
+  offset_x = x - last_offset_x;
+  offset_y = y - last_offset_y;
+  last_offset_x = x;
+  last_offset_y = y;
+
+  offset_x *= sensitivity;
+  offset_y *= sensitivity;
+
+  yaw += offset_x;
+  pitch -= offset_y;
+
+  // limit rotation of y axis
+  if (pitch > 89.0f)
+    pitch = 89.0f;
+  if (pitch < -89.0f)
+    pitch = -89.0f;
+
+  direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+  direction.y = sin(glm::radians(pitch));
+  direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+  front = glm::normalize(direction);
 }
 
 void CameraComponent::update(Uniform *uniform) {
--- src/astralix/src/modules/renderer/components/camera/camera-component.hpp
+++ src/astralix/src/modules/renderer/components/camera/camera-component.hpp
@@ -8,28 +8,41 @@ namespace astralix {
 
 class CameraComponent : public Component<CameraComponent> {
 public:
-  CameraComponent(COMPONENT_INIT_PARAMS, glm::vec3 position);
-  CameraComponent();
+  CameraComponent(COMPONENT_INIT_PARAMS);
 
   void update(Uniform *uniform);
 
   void use_perspective();
   void use_orthographic();
 
-  glm::vec3 get_position() { return m_position; };
-  glm::vec3 get_front() { return m_front; };
-
   glm::mat4 get_view_matrix() { return m_view_matrix; }
   glm::mat4 get_projection_matrix() { return m_projection_matrix; }
+
+  glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+  glm::vec3 front = glm::vec3(0.003012, -0.509046, 0.860734);
+  glm::vec3 rotation;
+
+  glm::vec3 direction;
+
+  float yaw = -90.0f;
+  float pitch = 0.0f;
+
+  float last_offset_x, last_offset_y;
+
+  float offset_x, offset_y;
+
+  bool is_first_recalculation = true;
+
+  float speed = 2.0f;
+  float sensitivity = 0.1f;
+
+  void recalculate_camera_rotation(float x, float y);
 
 private:
   void recalculate_projection_matrix();
   void recalculate_view_matrix();
 
-  glm::vec3 m_up;
-  glm::vec3 m_front;
   unsigned int m_shader_render_id;
-  glm::vec3 m_position;
 
   glm::mat4 m_view_matrix;
   glm::mat4 m_projection_matrix;
