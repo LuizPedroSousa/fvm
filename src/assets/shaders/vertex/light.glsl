47aaf5b74351e0d62007d4989d0cc3e33f6a0ae5
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-07-02 17:36:25 -0300

    chore(shaders/vertex/light.glsl): fix conventional name of texture coordinates :lipstick:

--- src/assets/shaders/vertex/light.glsl
+++ src/assets/shaders/vertex/light.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 normal;
-layout(location = 2) in vec2 texture_cord;
+layout(location=0)in vec3 position;
+layout(location=1)in vec3 normal;
+layout(location=2)in vec2 texture_coordinates;
 
-out OBJECT_COORDINATES {
+out OBJECT_COORDINATES{
   vec2 texture;
   vec3 normal;
   vec3 fragment;
@@ -15,11 +15,11 @@ uniform mat4 view;
 uniform mat4 projection;
 uniform mat4 model;
 
-void main() {
-  obj_coordinates.fragment = vec3(model * vec4(position, 1.));
-  mat3 normal_matrix       = mat3(transpose(inverse(model)));
-  obj_coordinates.normal   = normalize(normal_matrix * normal);
-  obj_coordinates.texture  = texture_cord;
-
-  gl_Position = projection * view * vec4(obj_coordinates.fragment, 1.);
+void main(){
+  obj_coordinates.fragment=vec3(model*vec4(position,1.));
+  mat3 normal_matrix=mat3(transpose(inverse(model)));
+  obj_coordinates.normal=normalize(normal_matrix*normal);
+  obj_coordinates.texture=texture_coordinates;
+  
+  gl_Position=projection*view*vec4(obj_coordinates.fragment,1.);
 }
