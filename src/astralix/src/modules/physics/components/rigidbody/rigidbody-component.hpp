6ab496ac6df63167e04bbb11f7688decd198d93e
LuizPedroSousa <luizpedrosousa64@gmail.com>
2025-02-17 18:34:26 -0300

    feat(physics-system): implement basic rigidbody and mesh collision with PhysX 🎇

--- src/astralix/src/modules/physics/components/rigidbody/rigidbody-component.hpp
+++ src/astralix/src/modules/physics/components/rigidbody/rigidbody-component.hpp
@@ -1,22 +1,30 @@
 #pragma once
+#include "PxPhysics.h"
+#include "PxSceneDesc.h"
 #include "ecs/components/component.hpp"
 
 namespace astralix {
-  class RigidBodyComponent : public Component<RigidBodyComponent>
-  {
-  public:
-    RigidBodyComponent(COMPONENT_INIT_PARAMS, float gravity = 0.5f,
-      float velocity = 2.0f, float acceleration = 2.0f,
-      float drag = 0.0f, float mass = 1.0f);
 
-    void update(float dt);
+enum RigidType { Static = 0, Dynamic = 1 };
 
-  private:
-    float velocity;
-    float gravity;
-    float drag;
-    float mass;
-    float acceleration;
-  };
+class RigidBodyComponent : public Component<RigidBodyComponent> {
+public:
+  RigidBodyComponent(COMPONENT_INIT_PARAMS,
+                     RigidType rigid_type = RigidType::Dynamic,
+                     float gravity = 0.5f, float velocity = 2.0f,
+                     float acceleration = 2.0f, float drag = 0.0f,
+                     float mass = 1.0f);
+
+  void start(physx::PxPhysics *physics, physx::PxScene *scene);
+  void update(float dt);
+
+private:
+  float velocity;
+  float gravity;
+  float drag;
+  float mass;
+  float acceleration;
+  RigidType m_rigid_type;
+};
 
 } // namespace astralix
