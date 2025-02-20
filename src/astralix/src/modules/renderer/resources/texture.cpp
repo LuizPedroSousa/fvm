0aac0e1256eb135f1c414237dd351cacb119ce0c
LuizPedroSousa <luizpedrosousa64@gmail.com>
2025-02-20 04:31:07 -0300

    fix(resources/texture): adjust local project texture path mapping 🐛

--- src/astralix/src/modules/renderer/resources/texture.cpp
+++ src/astralix/src/modules/renderer/resources/texture.cpp
@@ -3,6 +3,7 @@
 #include "engine.hpp"
 #include "filesystem"
 #include "glad/glad.h"
+#include "managers/project-manager.hpp"
 #include "platform/OpenGL/opengl-texture2D.hpp"
 #include "platform/OpenGL/opengl-texture3D.hpp"
 #include "stb_image/stb_image.h"
@@ -11,70 +12,72 @@
 
 namespace astralix {
 
-  void Texture::free_image(u_char* data) { stbi_image_free(data); }
+void Texture::free_image(u_char *data) { stbi_image_free(data); }
 
-  int Texture::get_image_format(int nr_channels) {
-    switch (nr_channels) {
-    case 1:
-      return GL_RED;
-    case 3:
-      return GL_RGB;
+int Texture::get_image_format(int nr_channels) {
+  switch (nr_channels) {
+  case 1:
+    return GL_RED;
+  case 3:
+    return GL_RGB;
 
-    default:
-      return GL_RGBA;
-    }
+  default:
+    return GL_RGBA;
+  }
+}
+
+std::filesystem::path Texture::get_path(const std::string &path) {
+  auto project = ProjectManager::get()->get_active_project();
+
+  auto base_path =
+      std::filesystem::path(project->get_config().resources.directory)
+          .append(path);
+
+  return base_path;
+};
+
+Image Texture::load_image(std::string filename, bool flip_image_on_loading) {
+
+  int width, height, nr_channels;
+
+  unsigned char *data;
+
+  auto path = get_path(filename.c_str());
+
+  stbi_set_flip_vertically_on_load(flip_image_on_loading);
+
+  data = stbi_load(path.c_str(), &width, &height, &nr_channels, 0);
+
+  if (!data) {
+    free_image(data);
+    ASTRA_EXCEPTION(true, "Cant't load image");
   }
 
-  std::filesystem::path Texture::get_path(const std::string& path) {
-    return std::filesystem::current_path()
-      .parent_path()
-      .append("src")
-      .append("assets")
-      .append(path.c_str());
-  };
+  return Image{width, height, nr_channels, data};
+};
 
-  Image Texture::load_image(std::string filename, bool flip_image_on_loading) {
+Ref<Texture2D> Texture2D::create(const ResourceID &resource_id,
+                                 const std::string &path,
+                                 const bool flip_image_on_loading) {
 
-    int width, height, nr_channels;
-
-    unsigned char* data;
-
-    auto path = get_path(filename.c_str());
-
-    stbi_set_flip_vertically_on_load(flip_image_on_loading);
-
-    data = stbi_load(path.c_str(), &width, &height, &nr_channels, 0);
-
-    if (!data) {
-      free_image(data);
-      ASTRA_EXCEPTION(true, "Cant't load image");
-    }
-
-    return Image{ width, height, nr_channels, data };
-  };
-
-  Ref<Texture2D> Texture2D::create(const ResourceID& resource_id,
-    const std::string& path,
-    const bool flip_image_on_loading) {
-
-    return create_renderer_component_ref<Texture2D, OpenGLTexture2D>(
+  return create_renderer_component_ref<Texture2D, OpenGLTexture2D>(
       Engine::get()->renderer_api->get_api(), resource_id,
-      TextureConfig{ .load_image = LoadImageConfig{
+      TextureConfig{.load_image = LoadImageConfig{
                         .path = path,
                         .flip_image_on_loading = flip_image_on_loading,
-                    } });
-  };
+                    }});
+};
 
-  Ref<Texture2D> Texture2D::create(const ResourceID& resource_id,
-    TextureConfig config) {
-    return create_renderer_component_ref<Texture2D, OpenGLTexture2D>(
+Ref<Texture2D> Texture2D::create(const ResourceID &resource_id,
+                                 TextureConfig config) {
+  return create_renderer_component_ref<Texture2D, OpenGLTexture2D>(
       Engine::get()->renderer_api->get_api(), resource_id, config);
-  };
+};
 
-  Ref<Texture3D> Texture3D::create(const ResourceID& resource_id,
-    const std::vector<std::string>& faces_path) {
-    return create_renderer_component_ref<Texture3D, OpenGLTexture3D>(
+Ref<Texture3D> Texture3D::create(const ResourceID &resource_id,
+                                 const std::vector<std::string> &faces_path) {
+  return create_renderer_component_ref<Texture3D, OpenGLTexture3D>(
       Engine::get()->renderer_api->get_api(), resource_id, faces_path);
-  };
+};
 
 } // namespace astralix
