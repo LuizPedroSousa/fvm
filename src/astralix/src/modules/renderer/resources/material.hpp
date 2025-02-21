cd9cf2d7b625235411d8e4b1b04e5f948bd67964
LuizPedroSousa <luizpedrosousa64@gmail.com>
2025-02-20 23:58:13 -0300

    refact(material): add normal_map ♻️

--- src/astralix/src/modules/renderer/resources/material.hpp
+++ src/astralix/src/modules/renderer/resources/material.hpp
@@ -17,10 +17,10 @@ public:
            std::vector<ResourceID> specular,
            std::optional<ResourceID> normal_map);
 
-  static Ref<Material> create(ResourceID id,
-                              std::vector<ResourceID> diffuse_ids,
-                              std::vector<ResourceID> specular_ids,
-                              std::optional<ResourceID> normal_map);
+  static Ref<Material>
+  create(ResourceID id, std::vector<ResourceID> diffuse_ids = {},
+         std::vector<ResourceID> specular_ids = {},
+         std::optional<ResourceID> normal_map = std::nullopt);
 };
 
 } // namespace astralix
